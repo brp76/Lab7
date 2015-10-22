311f61fd46ab45c0c01e69386f3c5f7873c97c2a
Ben Posey <brp76@pitt.edu>
2015-10-22 11:48:26 -0400

    Print array added

--- src/Lab7.java
+++ src/Lab7.java
@@ -1,4 +1,31 @@
 
 public class Lab7 {
+    public static void printArray(int[] arr, int numItems) {
+	    System.out.print("[ ");
+	    for (int j=0; j < (arr.length - 1); j++) {
+	        if (j >= numItems) {
+	        	System.out.print("***, ");
+	        } else {
+	        	System.out.print(arr[j] + ", ");
+	        }
+	    }
+	
+	    if ((arr.length -1) >= numItems) {
+	        System.out.println("*** ]");
+	    } else {
+	        System.out.println(arr[arr.length - 1] + " ]");
+	    }
+    }
+    
+	public static void main(String[] args) {
+		int [] arr = new int[10];
+		int numItems = 0, seasons = 10;
+		for (int i = 0; i < seasons; i ++){
+			System.out.println("Season: " + (i+1));
+			arr[i]=i;
+			printArray(arr,i);
+		}
+		
+	}
 
 }
