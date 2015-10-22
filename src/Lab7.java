8bf6ae2235ae8edd6605803c3e962acbfab42269
Ben Posey <brp76@pitt.edu>
2015-10-22 13:16:17 -0400

    Added comments, cleaned up code

--- src/Lab7.java
+++ src/Lab7.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class Lab7 {
+	// Global variables
 	public static int numRutP = 0, numRutM = 0, resizeP = 0, resizeM = 0;
+	
     public static void printArray(int[] arr, int numItems) {
 	    System.out.print("[ ");
 	    for (int j=0; j < (arr.length - 1); j++) {
@@ -21,10 +23,10 @@ public class Lab7 {
     
     public static int[] fillArrayP(int [] arr, int numItems, int location) {
     	for (int j = location; j < (numItems + location); j++ ) {
-    		if (numRutP == arr.length) {
+    		if (numRutP == arr.length) { // If there are more rutabagas than size of array, resize
     		    arr = resizeP(arr);
     		}
-    		arr[j] = numItems;
+    		arr[j] = numItems; // Fill array with rutabagas for that many times
     		numRutP ++;
     	}
     	return arr;
@@ -32,10 +34,10 @@ public class Lab7 {
     
     public static int[] fillArrayM(int [] arr, int numItems, int location) {
     	for (int j = location; j < (numItems + location); j++ ) {
-    		if (numRutM == arr.length) {
+    		if (numRutM == arr.length) { // If there are more rutabagas than size of array, resize
     		    arr = resizeM(arr);
     		}
-    		arr[j] = numItems;
+    		arr[j] = numItems; // Fill array with rutabagas for that many times
     		numRutM ++;
     	}
     	return arr;
@@ -43,10 +45,10 @@ public class Lab7 {
     
     public static int[] resizeP(int [] oldArray) {
     	int oldLength = oldArray.length;
-    	int newLength = oldLength + 2;
+    	int newLength = oldLength + 2; // Add 2 locations for array
     	int [] newArray = new int[newLength];
     	
-    	for (int j = 0; j < oldLength; j++) {
+    	for (int j = 0; j < oldLength; j++) { // Fill new array with old data
     		newArray[j] = oldArray[j];
     	}
     	System.out.printf("\tResized Pangloss's Garden to %d!\n", newLength);
@@ -56,10 +58,10 @@ public class Lab7 {
     
     public static int[] resizeM(int [] oldArray) {
     	int oldLength = oldArray.length;
-    	int newLength = oldLength * 2;
+    	int newLength = oldLength * 2; // Multiply array size by 2
     	int [] newArray = new int[newLength];
     	
-    	for (int j = 0; j < oldLength; j++) {
+    	for (int j = 0; j < oldLength; j++) { // Fill in data
     		newArray[j] = oldArray[j];
     	}
     	System.out.printf("\tResized Martin's Garden to %d!\n", newLength);
@@ -74,9 +76,11 @@ public class Lab7 {
 		Random r = new Random();
 		
 		for (int i = 0; i < seasons; i ++) {
-			numItems += rutabaga;
-			rutabaga = r.nextInt(5);
+			numItems += rutabaga; // Useful for finding location in fillArray functions
+			rutabaga = r.nextInt(5); // Random # between 0 and 4
+			
 			System.out.println("Season: " + (i+1) + ", " + rutabaga + " rutabaga(s)");
+			
 			arrPangloss = fillArrayP(arrPangloss, rutabaga, numItems);
 			arrMartin = fillArrayM(arrMartin, rutabaga, numItems);
 			
